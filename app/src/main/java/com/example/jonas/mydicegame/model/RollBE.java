5ce737c3b35e312e21584a904ee693d88459e20e
jbitsme <jonas_husted@live.dk>
2018-02-26 15:13:40 +0100

    hvorfor

---
+++ app/src/main/java/com/example/jonas/mydicegame/model/RollBE.java
@@ -0,0 +1,12 @@
+package com.example.jonas.mydicegame.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class RollBE implements Serializable {
+
+    int roll1;
+    int roll2;
+
+    Date time;
+}
